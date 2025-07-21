ca9ce4d652b8b479078f067c8859fd4dc403ce28
Carson184 <25065558@imail.sunway.edu.my>
2025-07-21 05:56:27 +0000

    day 1

---
+++ Part1/ex03.py
@@ -0,0 +1,17 @@
+# Original text
+string1 = 'MaRviN THE bOt'
+
+#convert to lowercase
+string_lower = "Jia Hao"
+
+#convert to uppercase 
+string_upper = "is"
+
+#remove spaces
+string_nospace = "gay"
+
+# Print results
+print(string1)
+print(string_lower)
+print(string_upper)
+print(string_nospace)
---
+++ Part1/ex05.py
@@ -0,0 +1,12 @@
+# Word variables
+word1 = "Jia "
+word2 = "is "
+text1 = "figuring  "
+word3 = "out"
+
+# TODO: Combine the words into one sentence and display on screen
+# Make sure your sentence reads: "the car exceeded the speed limit"
+
+# Insert your code here
+text2 =word1 + word2 + text1 + word3
+print(text2)
