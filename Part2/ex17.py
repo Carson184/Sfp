f282c9bfc496826f4cf1f51447900af6a50b9a4c
Carson184 <25065558@imail.sunway.edu.my>
2025-07-21 12:05:56 +0000

    Day 1 & 2

---
+++ Part2/ex17.py
@@ -0,0 +1,7 @@
+# TODO: Refer to the objective and sample output and figure out your own code!
+# Time to graduate :p
+import random
+
+name = input("What is your name?\nMarvin")
+print(f"{name}Marvin, your codename is: Fearless Dragon")
+print(f"Your lucky number is: {random.randint(1, 100)}")
